4ea2614ac9ff1c6a3d0f8074c3a949c7bcfb9804
rg872 <gilang0293@gmail.com>
2021-05-16 08:09:52 +0700

    init all apps

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  projects: ['<rootDir>/apps/web-next'],
+  projects: [
+    '<rootDir>/apps/web-next',
+    '<rootDir>/apps/desktop-react',
+    '<rootDir>/apps/phone-react-native',
+    '<rootDir>/apps/desktop-electron',
+  ],
 };
